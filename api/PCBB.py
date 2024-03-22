139351d0ab5a3223f1fa35ebd8ba859f01d31002
AryanRanderiyaWorkstation <aryan.k.randeriya@gmail.com>
2024-03-22 17:02:27 +0530

    Improved PCBB.py

--- api/PCBB.py
+++ api/PCBB.py
@@ -3,15 +3,17 @@ import random
 import time
 import copy
 
+# Constants
+EMPTY = 0
+BUFFER_SIZE = 5
+
 
 class Monitor:
     """
     Synchronization tool for monitoring producer and consumer and ensuring they wait and signal before producing / consuming from the bounded.
     """
 
-    BUFFER_SIZE = 5  # Maximum Size of the bounded buffer
     buffer = []  # Shared buffer
-    monitor_busy = True  # The current status of the monitor
 
     # Lock object for the Monitor from which synchronisation will occur
     monitor_lock = threading.Lock()
@@ -20,160 +22,131 @@ class Monitor:
     condition_vars = threading.Condition(monitor_lock)
 
     def __init__(self):
-        self._buffer_list = []  # Maintain history of all the buffers
-        self._produce_consumed_list = []  # Maintain history of all produced/consumed items
+        self._buffer_history = []  # Maintain history of all the buffers
+        self._produce_consumed = []  # Maintain history of all produced/consumed items
+        self._buffer_states = []  # Maintain history of all produced/consumed items
 
     @property
-    def buffer_list(self):
-        return self._buffer_list
-
-    def add_buffer_list(self, buffer: list):
-
-        if not buffer:
-            buffer = "Buffer is Empty"
-        else:
-            buffer = ", ".join(buffer)
-
-        self._buffer_list.append(buffer)
+    def buffer_list(self) -> list:
+        return self._buffer_history
 
     @property
-    def produce_consumed_list(self):
-        return self._produce_consumed_list
+    def produce_consumed_list(self) -> list:
+        return self._produce_consumed
 
-    def add_prod_cons_list(self, obj: str):
-        self._produce_consumed_list.append(obj)
+    def add_buffer_list(self, buffer: list) -> None:
+        buffer = ", ".join(buffer) if buffer else "Buffer is Empty"
+        self._buffer_history.append(buffer)
 
-    def wait(self) -> None:
+    def add_prod_cons_list(self, item: str) -> None:
+        self._produce_consumed.append(item)
+
+    def add_buffer_state_list(self, state: str) -> None:
+        self._buffer_states.append(state)
+
+    def append(self, item_name: int) -> None:
         """
-        Monitor method to ensure that the process will wait if the Monitor is busy and currently occupied.
+        Monitor public method for consumer to access
+        """
+
+        with self.monitor_lock:
+
+            buffer_count = len(self.buffer)  # Number of elements in the buffer
+
+            if buffer_count == BUFFER_SIZE:  # If the Buffer is full then wait
+                print("Buffer FULL, Producer is waiting...")
+                print('------------------------------------------------')
+                self.condition_vars.wait()
+
+            self.buffer.append(item_name)  # Insert new item into buffer
+
+            self.add_buffer_list(copy.deepcopy(self.buffer))
+            self.add_prod_cons_list(f"Produced {item_name}")
+
+            self.condition_vars.notify()  # Notify the monitor that it is free
+
+            print(f"Produced {item_name}, Buffer: {self.buffer}")
+            print('------------------------------------------------')
+
+    def take(self) -> None:
+        """
+        Monitor public method for consumer to access
         """
         with self.monitor_lock:
-            # with keyword helps with resource management "Entry & Exit methods"
-            # & for error handling
-            while self.monitor_busy:
-                self.condition_vars.wait()  # Wait until monitor is free
-            self.monitor_busy = True
 
-    def signal(self) -> None:
-        """
-        Monitor method to send a signal in the consumer and producer methods to notify that the Monitor is now free.
-        """
-        with self.monitor_lock:
-            self.monitor_busy = False
-            self.condition_vars.notify()  # Signal that monitor is free
-    # "Append" method
+            buffer_count = len(self.buffer)  # Number of elements in the buffer
 
-    def produce(self, item_index: str, seconds: float) -> None:  # Insert into buffer
-        """
-        The producer inserts items into the bounded buffer and ensures synchronisation by using wait and signal methods
-        of the monitor.
-        """
+            if (buffer_count) == EMPTY:  # Ensure buffer is not empty
+                print("Buffer EMPTY, Consumer is waiting...")
+                print('------------------------------------------------')
+                self.condition_vars.wait()
 
-        item_index = "P" + item_index  # Format the item name
+            # Remove item from buffer
+            item = self.buffer.pop(0)
 
-        buffer_count = len(self.buffer)  # Number of elements in the buffer
+            # Notify monitor that it is now free
+            self.condition_vars.notify()
 
-        if buffer_count == self.BUFFER_SIZE:  # If the Buffer is full then wait
-            self.wait()
+            self.add_buffer_list(copy.deepcopy(self.buffer))
+            self.add_prod_cons_list(f"Consumed {item}")
 
-        # Halt execution temporarily to simulate processing
-        time.sleep(seconds)
+            print(f"Consumed {item}, Buffer: {self.buffer}")
+            print('------------------------------------------------')
 
-        self.buffer.append(item_index)  # Insert new item into buffer
-
-        self.add_buffer_list(copy.deepcopy(self.buffer))
-        self.add_prod_cons_list(f"Produced {item_index}")
-        # self.buffer_list = copy.deepcopy(self.buffer)
-        # self.produce_consumed_list = f"Produced {item_index}"
-
-        self.signal()  # Notify the monitor that it is free
-
-        # print(f"Produced {item_index}, Buffer: {self.buffer}")
-        # print('------------------------------------------------')
-
-    def consume(self, seconds: float) -> None:
-        """
-        Consumer that pops items from the buffer, decrements the length (count--), and signals that monitor is free
-        after deletion of item.
-        """
-
-        # Halt execution temporarily to simulate processing
-        time.sleep(seconds)
-
-        if len(self.buffer) == 0:  # Ensure buffer is not empty
-            self.wait()  # If buffer is empty then wait indefinitely until items are added
-
-        # Remove item from buffer
-        item = self.buffer.pop(0)
-
-        # Notify monitor that it is now free
-        self.signal()
-
-        self.add_buffer_list(copy.deepcopy(self.buffer))
-        self.add_prod_cons_list(f"Consumed {item}")
-        # self.buffer_list = copy.deepcopy(self.buffer)
-        # self.produce_consumed_list = f"Consumed {item}"
-
-        # self.add_prod_cons_list(
-        #     f"Number of Items left in Buffer: {len(self.buffer)}")
-
-        # print(f"Consumed {item}, Buffer: {self.buffer}")
-        # print(f"Number of Items left in Buffer: {len(self.buffer)}")
-        # print('------------------------------------------------')
+    # ! End of class ^
 
 
-def produce_consume(item_index: int, process_times: list, monitor: Monitor) -> None:
+def producer(seconds: float, monitor: Monitor, no_of_processes: int) -> None:  # Insert into buffer
     """
-    Method that calls both producer and consumer methods.
-    This method will be a thread called in "process_as_begin"
+    The producer inserts items into the bounded buffer and ensures synchronisation by using wait and signal methods
+    of the monitor.
     """
-    # seconds = process_times[item_index]
-    seconds = random.uniform(0, 2.5)
-    # seconds = 0
-
-    item_index = str(item_index + 1)
-
-    monitor.produce(item_index, seconds)
-    monitor.consume(seconds)  # monitor = self
+    for count in range(no_of_processes):
+        item_name: str = "P" + str(count)  # Format the item name
+        monitor.append(item_name)
 
 
-def process_as_begin(number_processes: int, times: list, monitor: Monitor) -> None:  # process as begin
+def consumer(seconds: float, monitor: Monitor, no_of_processes: int) -> None:
+    """
+    Consumer that pops items from the buffer, decrements the length (count--), and signals that monitor is free
+    after deletion of item.
+    """
+
+    for _ in range(no_of_processes):
+        monitor.take()
+
+
+def process_as_begin(no_of_processes: int, monitor=Monitor()) -> None:  # process as begin
     """
     Begin process of producer & consumer.
     """
 
-    threads_list = []
+    thread1 = threading.Thread(
+        target=producer, args=(0.5, monitor, no_of_processes,))
 
-    # Iterate and create new process threads until number of processes.
-    for num in range(number_processes):
-        thread = threading.Thread(
-            target=produce_consume, args=(num, times, monitor))
-        # Call produce_consume() in the thread, pass current process no. & times[] for execution seconds to the method
-        thread.start()
-        # Insert thread into the list, to join it later
-        threads_list.append(thread)
+    thread2 = threading.Thread(target=consumer, args=(
+        0.5, monitor, no_of_processes,))
 
-    for thread in threads_list:  # Join all the threads
-        thread.join()  # Wait for each thread to finish executing before continuing to the rest of the program
+    thread1.start()
+    thread2.start()
+
+    # Wait for each thread to finish executing before continuing to the rest of the program
+    thread1.join()
+    thread2.join()
 
 
 def main() -> None:
-    process_times = []
 
     while True:
-        number_processes = int(input('Enter Number of Processes: '))
+        no_of_processes = int(input('Enter No. of Processes: '))
         print('------------------------------------------------')
 
-        # for process_no in range(number_processes):
-        #     time = int(input(f"Enter time for Process {process_no + 1}: "))
-        #     processtimes.insert(process_no, time)  # process_no = index
-
-        if number_processes > 1:
-            process_as_begin(number_processes, process_times)
+        if no_of_processes > 0:
+            process_as_begin(no_of_processes)
             print("All items produced and consumed.")
             break
         else:
-            print("Number of processes must be greater than 1")
+            print("Number of processes must be greater than 0")
 
 
 if __name__ == '__main__':
